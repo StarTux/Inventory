ad35bd08498b478546d9d4c5df9607ffe6c6e27e
StarTux <ths.loy@googlemail.com>
2022-02-11 00:02:32 +0100

    Allow shulker boxes in the stash

--- src/main/java/com/cavetale/inventory/StashCommand.java
+++ src/main/java/com/cavetale/inventory/StashCommand.java
@@ -6,25 +6,19 @@ import com.cavetale.inventory.gui.Gui;
 import com.cavetale.inventory.sql.SQLStash;
 import com.cavetale.inventory.storage.InventoryStorage;
 import com.cavetale.inventory.util.Items;
-import com.cavetale.mytems.Mytems;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
-import org.bukkit.Tag;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import static net.kyori.adventure.text.Component.join;
 import static net.kyori.adventure.text.Component.text;
-import static net.kyori.adventure.text.JoinConfiguration.noSeparators;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 @RequiredArgsConstructor
@@ -68,13 +62,13 @@ public final class StashCommand implements CommandExecutor {
                 plugin.database.insert(row);
             } catch (Exception e) {
                 Bukkit.getScheduler().runTask(plugin, () -> {
-                        player.sendMessage(ChatColor.RED + "Your stash is unavailable. Please try again later.");
+                        player.sendMessage(text("Your stash is unavailable. Please try again later.", RED));
                     });
                 throw new IllegalStateException(row.toString(), e);
             }
             if (row.getId() == null) {
                 Bukkit.getScheduler().runTask(plugin, () -> {
-                        player.sendMessage(ChatColor.RED + "Your stash is unavailable. Please try again later.");
+                        player.sendMessage(text("Your stash is unavailable. Please try again later.", RED));
                     });
                 throw new IllegalStateException("failed to save: " + row);
             }
@@ -100,7 +94,7 @@ public final class StashCommand implements CommandExecutor {
                 .atomic("version", row.getVersion() + 1)
                 .sync();
             if (success <= 0) {
-                player.sendMessage(ChatColor.RED + "Your stash is unavailable. Please try again later.");
+                player.sendMessage(text("Your stash is unavailable. Please try again later.", RED));
                 throw new IllegalStateException("atomic update failed: " + row);
             }
         } else {
@@ -120,19 +114,6 @@ public final class StashCommand implements CommandExecutor {
     }
 
     private void onClose(Player player, SQLStash row, Gui gui) {
-        for (int i = 0; i < gui.getInventory().getSize(); i += 1) {
-            ItemStack itemStack = gui.getInventory().getItem(i);
-            if (itemStack == null || itemStack.getType() == Material.AIR) continue;
-            if (Mytems.forItem(itemStack) != null) continue;
-            if (Tag.SHULKER_BOXES.isTagged(itemStack.getType())) {
-                gui.getInventory().setItem(i, null);
-                Items.give(player, itemStack);
-                player.sendMessage(join(noSeparators(),
-                                        text("You cannot stash "),
-                                        Component.translatable(itemStack),
-                                        text("!")).color(RED));
-            }
-        }
         InventoryStorage inventoryStorage = InventoryStorage.of(gui.getInventory());
         row.setJson(Json.serialize(inventoryStorage));
         row.setAccessNow();
@@ -150,7 +131,7 @@ public final class StashCommand implements CommandExecutor {
             e.printStackTrace();
         }
         if (success <= 0) {
-            player.sendMessage(ChatColor.RED + "Your stash is unavailable. Please try again later.");
+            player.sendMessage(text("Your stash is unavailable. Please try again later.", RED));
             List<ItemStack> list = new ArrayList<>();
             for (int i = 0; i < gui.getInventory().getSize(); i += 1) {
                 ItemStack itemStack = gui.getInventory().getItem(i);
